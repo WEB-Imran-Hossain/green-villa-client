e8ec861d81db3ebf84bd3a071936692b3d297234
Imran <webimran2021@gmail.com>
2023-11-10 20:00:05 +0600

    responsive special offers section and map location

--- src/Pages/RoomsPage/Rooms.jsx
+++ src/Pages/RoomsPage/Rooms.jsx
@@ -5,29 +5,42 @@ const Rooms = () => {
   const [rooms, setRooms] = useState([]);
 
   useEffect(() => {
-      fetch('http://localhost:5000/rooms')
-          .then(res => res.json())
-          .then(data => setRooms(data))
-  }, [])
+    fetch("http://localhost:5000/rooms")
+      .then((res) => res.json())
+      .then((data) => setRooms(data));
+  }, []);
 
   return (
     <div className="mt-32 max-w-[90vw] mx-auto">
       <div className="p-4 mt-40 lg:mt-0">
         <div className="space-y-5 mb-10">
-          <h4 className="text-xl font-bold text-[#32CD32]">LUXURY EXPERIENCE</h4>
-          <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">Rooms & Suites
+          <h4 className="text-xl font-bold text-[#32CD32]">
+            LUXURY EXPERIENCE
+          </h4>
+          <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">
+            Rooms & Suites
+            <p className="py-4 text-base font-normal text-[#737373]">
+              Welcome to our <strong>Green Villa</strong> hotel, where luxury and comfort
+              converge to create an unparalleled experience for our esteemed
+              guests. Our meticulously designed hotel rooms and suites are a
+              testament to refined elegance, offering a sanctuary of
+              sophistication in the heart of city area. Whether you're here for
+              business or leisure, our accommodations are tailored to meet the
+              diverse needs of today's discerning travelers.
+            </p>
           </h5>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {
-            rooms.map(room => <RoomCard key={room._id} room={room}></RoomCard>)
-          }
+          {rooms.map((room) => (
+            <RoomCard key={room._id} room={room}></RoomCard>
+          ))}
         </div>
         <div className="text-center mt-12">
-          <button className="btn hover:bg-transparent text-white bg-[#FF3811] border-[#FF3811] hover:border-[#32CD32] hover:text-[#32CD32] text-lg font-semibold rounded-none normal-case">View All Rooms</button>
+          <button className="btn hover:bg-transparent text-white bg-[#FF3811] border-[#FF3811] hover:border-[#32CD32] hover:text-[#32CD32] text-lg font-semibold rounded-none normal-case">
+            View All Rooms
+          </button>
         </div>
       </div>
-
     </div>
   );
 };
--- src/Pages/SpecialOfferPage/SpecialOffer.jsx
+++ src/Pages/SpecialOfferPage/SpecialOffer.jsx
@@ -1,48 +1,66 @@
 const SpecialOffer = () => {
-    return (
-        
-        <div className="max-w-[60vw] mx-auto lg:w-4/5 space-y-4 space-y-4">
-            <h4 className="text-xl font-bold text-[#32CD32]">OFFERS</h4>
-        <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">Green Villa Special Offers</h5>
-        <p className="py-4 text-base font-normal text-[#737373]">One of the best offers for unique holidays is <strong>Green Villa</strong> hotel planning. <strong>Green Villa</strong> tailored to your specific desires take into account your interests, budget, and preferred destinations, ensuring that every aspect of your trip is personalized.
-        </p>
-            <div>
-            <div className="card card-side bg-base-100 shadow-xl rounded-none">
-                <figure><img src="https://i.ibb.co/LtRftNz/1000x625.jpg" /></figure>
-                <div className="card-body">
-                    <div >
-                       
-                        <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">Stay 3 Nights - Save 20%</h5>
-                        <p className="py-4 text-base font-normal text-[#737373]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                        </p>
-                    </div>
-                </div>
+  return (
+    <div className=" max-w-[90vw] mx-auto space-y-4 space-y-4">
+      <h4 className="text-xl font-bold text-[#32CD32]">OFFERS</h4>
+      <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">
+        Green Villa Special Offers
+      </h5>
+      <p className="py-4 text-base font-normal text-[#737373]">
+        One of the best offers for unique holidays is{" "}
+        <strong>Green Villa</strong> hotel planning.{" "}
+        <strong>Green Villa</strong> tailored to your specific desires take into
+        account your interests, budget, and preferred destinations, ensuring
+        that every aspect of your trip is personalized.
+      </p>
+      <div className="">
+        <div className="card lg:card-side bg-base-100 shadow-xl rounded-none hover:bg-gradient-to-r from-[#32CD32] to-[rgba(21, 21, 21, 0.8)] flex flex-col lg:flex-row">
+          <figure>
+            <img src="https://i.ibb.co/MPSLVdB/hero-home-2.jpg" />
+          </figure>
+          <div className="card-body shadow-xl ">
+            <div className="hover:text-white">
+              <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">
+                Stay 3 Nights - <strong className="text-[#32CD32]">Save 20%</strong>
+              </h5>
+              <p className="py-4 text-base font-normal text-[#737373]">
+              Immerse yourself in the allure of an extended stay, where 3 nights unlock a savings and heightened enjoyment. Picture yourself unwinding in the heart of ciry area, surrounded by the unique blend of modern convenience and timeless elegance that defines our hotel. With this special offer, you not only secure a luxurious retreat but also enjoy a substantial 20% discount on your entire stay.
+              </p>
             </div>
-            <div className="card card-side bg-base-100 shadow-xl flex-row-reverse rounded-none">
-                <figure><img src="https://i.ibb.co/fY8vbCn/local-amenities-1-1.jpg" /></figure>
-                <div className="card-body">
-                    <div >
-                       
-                        <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">Typical Food</h5>
-                        <p className="py-4 text-base font-normal text-[#737373]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="card card-side bg-base-100 shadow-xl rounded-none">
-                <figure><img src="https://i.ibb.co/wQ6X0BZ/local-amenities-3-1.jpg" /></figure>
-                <div className="card-body">
-                    <div >
-                       
-                        <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">Horse Ride</h5>
-                        <p className="py-4 text-base font-normal text-[#737373]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            </div>
+          </div>
         </div>
-    );
+        <div className="card card-side bg-base-100 shadow-xl flex-row-reverse rounded-none hover:bg-gradient-to-r from-[#32CD32] to-[rgba(21, 21, 21, 0.8)] flex flex-col lg:flex-row-reverse">
+          <figure>
+            <img src="https://i.ibb.co/TL6rXyD/local-amenities-1-1.jpg" />
+          </figure>
+          <div className="card-body">
+            <div>
+              <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">
+                Typical Food
+              </h5>
+              <p className="py-4 text-base font-normal text-[#737373]">
+              Start your day with a traditional breakfast that captures the essence of our region.Indulge in fluffy<br/> pancakes drizzled with locally sourced honey, or savor a savory breakfast wrap<br/> filled with farm-fresh eggs, cheese, and a medley of aromatic spices. Wash it down with a cup of freshly brewed coffee or a refreshing herbal tea, setting the perfect tone for a day of exploration.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="card card-side bg-base-100 shadow-xl rounded-none hover:bg-gradient-to-r from-[#32CD32] to-[rgba(21, 21, 21, 0.8)] flex flex-col lg:flex-row">
+          <figure>
+            <img src="https://i.ibb.co/dcgG2Xf/bg-contact.jpg" />
+          </figure>
+          <div className="card-body">
+            <div>
+              <h5 className=" text-4xl md:text-5xl font-bold text-[#151515]">
+                Horse Ride
+              </h5>
+              <p className="py-4 text-base font-normal text-[#737373]">
+              The Horse Ride Package includes a curated riding experience tailored to your preferences.Choose from<br/> a leisurely ride for beginners, allowing you to connect with the gentle rhythm of your equine companion, <br/>or opt for a more adventurous trail for those seeking an adrenaline boost. As you meander through lush <br/>landscapes and scenic paths, you'll have the opportunity to witness the beauty of [Landmarks/Scenic Spots], creating lasting memories against the backdrop of nature's splendor.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SpecialOffer;
